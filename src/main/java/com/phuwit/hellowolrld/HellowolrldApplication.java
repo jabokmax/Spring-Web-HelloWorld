f76aef39fe0f92ed0526116496bcbbb7ec0e1872
Phuwit.n <jabokmax@outlook.com>
2017-08-19 14:07:53 +0700

    init project

---
+++ src/main/java/com/phuwit/hellowolrld/HellowolrldApplication.java
@@ -0,0 +1,12 @@
+package com.phuwit.hellowolrld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowolrldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowolrldApplication.class, args);
+	}
+}
---
+++ src/test/java/com/phuwit/hellowolrld/HellowolrldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.phuwit.hellowolrld;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HellowolrldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
